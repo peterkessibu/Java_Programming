a72f50df2dfc7bca8748b6ed35a157010b620c9b
Peter Essibu <essibupeterk10@gmail.com>
2024-07-01 13:34:19 +0000

    Updated

--- Main.java
+++ Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Create an InputStreamReader to read bytes and decode them into characters
-        java.io.InputStreamReader reader = new java.io.InputStreamReader(System.in);
-        // Create a StringBuilder to store the input
-        StringBuilder name = new StringBuilder();
-        
-        System.out.print("Enter your name: ");
-        
-        try {
-            // Read characters one by one from the input stream
-            int character;
-            while ((character = reader.read()) != '\n') {
-                // Append each character to the StringBuilder
-                name.append((char) character);
-            }
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
-
-        // Print the user's name back to the screen
+        String name = "Peter Essibu" ;
         System.out.println("Hello, " + name + "!");
     }
 }
